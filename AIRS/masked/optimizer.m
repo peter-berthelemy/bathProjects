4332a9d2515f2d60d10e1a8b0bf8c42c0d637b3a
peter-berthelemy <peter.berthelemy@gmail.com>
2023-07-15 13:52:06 +0100

    Created and tested optimiser

--- AIRS/masked/optimizer.m
+++ AIRS/masked/optimizer.m
@@ -1,19 +1,21 @@
 
-strans = struct('k', ST12.k, 'l', ST12.l);
-true = 
-results = struct('sum', zeros(10^4, 1), 'size', zeros(10^4, 1), 'blur', zeros(10^4, 1), 'wavelength', zeros(10^4, 1), 'ratio', zeros(10^4, 1));
-for sumCutoff = 1:10
+load(fullfile(cd, 'drawnTrue/2010_1_4_85_11.mat'))
+load(fullfile(cd, 'testWaves/2010_1_4_85.mat'))
+strans = struct('k', ST.k(:,:,11), 'l', ST.l(:,:,11));
+true = BW;
+results = struct('sum', [], 'size', [], 'blur', [], 'wavelength', [], 'ratio', []);
+for sumCutoff = 0.1:0.005:0.5
     sumCutoff
-    for sizeCutoff = 1:10
-        for blurCutoff = 1:10
-            for wavelengthCutoff = 1:10
+    for sizeCutoff = 50:5:300
+        for blurCutoff = 0.1:0.5:0.5
+            for wavelengthCutoff = 0:0.2:20
 
-                mask = thirdMask(strans, sumCutoff, sizeCutoff, blurCutoff, wavelengthCutoff, {'k', 'l'}, [5, 5, 1], 2);
-                results.sum = sumCutoff;
-                results.size = sizeCutoff;
-                results.blur= blurCutoff;
-                results.wavelength = wavelengthCutoff;
-                results.ratio = nnz(mask)/nnz(true);
+                mask = thirdMask(strans, sumCutoff, sizeCutoff, blurCutoff, wavelengthCutoff, {'k', 'l'}, [5, 5], 2);
+                results.sum(end+1) = sumCutoff;
+                results.size(end+1) = sizeCutoff;
+                results.blur(end+1)= blurCutoff;
+                results.wavelength(end+1) = wavelengthCutoff;
+                results.ratio(end+1) = nnz(mask)/nnz(true);
             end
         end
     end
