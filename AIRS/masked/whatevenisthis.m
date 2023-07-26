393c63a7660f2557c822d4818cd5c0662aacab9a
peter-berthelemy <peter.berthelemy@gmail.com>
2023-07-26 17:39:37 +0100

    Bits and bobs

--- AIRS/masked/whatevenisthis.m
+++ AIRS/masked/whatevenisthis.m
@@ -3,6 +3,8 @@
 strans = struct('k', ST.k(:,:,11), 'l', ST.l(:,:,11));
 mask = thirdMask(strans, results.sum(i), results.size(i), results.blur(i), results.wavelength(i), {'k', 'l'}, [5, 5], 2);
 
+
+mask = thirdMask(strans, 0.8, 150, 0.5, 50, {'k', 'l'}, [5, 5], 2);
 [m, mm] = ssim(mask, ST.WaveMask(:,:,11));
 
 figure;shat(ST.k(:,:,11))
